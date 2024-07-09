e8634ddd46e6335b716629b4f5102a59d4ea90f7
Janvi Chandra <janvichandra4@gmail.com>
2024-07-09 11:48:24 +0530

    Final Blog

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,49 +1,47 @@
 import { useState } from "react";
 
-const Create = () => {
+const Create = ({ onAddBlog }) => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Michael');
 
-    const [title,setTitle] = useState('');
-    const [body,setBody] = useState('');
-    const [author,setAuthor] = useState('Michael');
-
-
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const blog = {title,body,author};
-        console.log(blog);
+        const blog = { title, body, author, id: Date.now(), date: new Date().toISOString() };
+        onAddBlog(blog);
+        setTitle('');
+        setBody('');
+        setAuthor('Michael');
     }
-    
+
     return ( 
         <div className="create">
             <h2>Wanna create something together?</h2>
             <form onSubmit={handleSubmit}>
-
                 <label>Blog Title:</label>
                 <input
-                type="text" placeholder="Enter title"
-                required
-                value={title}
-                onChange={(e)=> setTitle(e.target.value)}
+                    type="text" placeholder="Enter title"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
-
                 <label>Blog Body:</label>
                 <textarea placeholder="Enter blog content"
-                required
-                value={body}
-                onChange={(e)=> setBody(e.target.value)}> </textarea>
-
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}> 
+                </textarea>
                 <label>Blog Author:</label>
                 <select value={author}
-                onChange={(e)=> setAuthor(e.target.value)}>
+                    onChange={(e) => setAuthor(e.target.value)}>
                     <option value="Emily">Emily</option>
                     <option value="Michael">Michael</option>
                     <option value="Sarah">Sarah</option>
                 </select>
-
                 <button className="AddBlog">Add Blog!</button>
             </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
